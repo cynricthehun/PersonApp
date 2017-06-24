d72d79be30deecf3024eaf88b83cd55a8372e95e
Joey Meyer <joeymeyer1986@gmail.com>
2017-06-24 17:49:07 -0500

    added a test for Removing a person and for providing a list of people.

---
+++ BLLUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BLLUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("BLLUnitTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c337bf43-30ad-4f74-8eff-e9ecde482ee4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BLLUnitTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL;
+using DAL;
+
+namespace BLLUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PeopleList_Provided()
+        {
+            // Arrange
+            List<Person> actual = PersonRepository.createPersonRepo();
+            List<Person> expected = new List<Person>();
+
+            // Act
+            expected.Add(new Person("Mahesh", "Chand", 35, "Male", "July 5"));
+
+            // Assert
+            Assert.AreEqual(actual[0].FirstName, expected[0].FirstName);
+        }
+
+        [TestMethod]
+        public void PeopleList_RemovePerson()
+        {
+            // Arrange
+            List<Person> actual = PersonRepository.createPersonRepo();
+            var expectedCount = 5; // five entries are in by default.
+            var actualCount = 0;
+
+            // Act
+            actual.Remove(actual[0]);
+            actualCount = actual.Count;
+
+            // Assert
+            Assert.AreNotEqual(actualCount, expectedCount);
+        }
+    }
+}
