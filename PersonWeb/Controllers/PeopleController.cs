4370e078a3222a96debed836b0a586df43e20bc5
Joey Meyer <joeymeyer1986@gmail.com>
2017-12-05 22:23:35 -0600

    added a ASP.net MVC application to the project. switched the default application from the UI Console application to the ASP.net MVC application. Added a index page to present the Person collection data.

---
+++ PersonWeb/Controllers/PeopleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+
+namespace PersonWeb.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            PersonBLL personBusinessLayerObject = new PersonBLL();
+
+            return View(personBusinessLayerObject);
+        }
+    }
+}
---
+++ PersonWeb/ViewModels/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace PersonWeb.ViewModels
+{
+    public class Person
+    {
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DisplayName("Age")]
+        public int Age { get; set; }
+        [DisplayName("Gender")]
+        public string Gender { get; set; }
+        [DisplayName("Birthday")]
+        public string Birthday { get; set; }
+    }
+}
